28b46ac088f694b36a7a5aa81082d4def1e6b7b2
Matheus Oliveira <97600307+MatheusOliveira-1@users.noreply.github.com>
2022-03-02 17:03:12 -0300

    iniciado projeto no Spring e o DER no MySql Workbench

---
+++ back_end/mindrestful/src/main/java/com/matheusoliveira/mindrestful/MindrestfulApplication.java
@@ -0,0 +1,13 @@
+package com.matheusoliveira.mindrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindrestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindrestfulApplication.class, args);
+	}
+
+}
---
+++ back_end/mindrestful/src/test/java/com/matheusoliveira/mindrestful/MindrestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusoliveira.mindrestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindrestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
